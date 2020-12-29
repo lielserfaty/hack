0303b8f6944c9c0e8381efb821136b1c9058fe19
lielserfaty <73876370+lielserfaty@users.noreply.github.com>
2020-12-29 22:10:28 +0200

    Add files via upload

---
+++ client.py
@@ -0,0 +1,54 @@
+import socket
+import msvcrt
+import threading
+from _thread import start_new_thread
+import time
+
+
+def run_client():
+    print("Client started, listening for offer requests...")
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+    # Enable broadcasting mode
+    client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    client.bind(("", 13117))
+    while True:
+        data, addr = client.recvfrom(1024)
+        print("Received offer from 172.1.0.4, attempting to connect...")
+        # check valid udp-message
+        break
+    connect_to_server(addr, data)
+
+
+def connect_to_server( addr, data):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((addr[0], int(data.decode().split(" ")[2])))
+    message_to_server="efrat"
+    client_socket.send(message_to_server.encode("utf-8")+'\n'.encode("utf-8"))
+    # get message from server about start the game and print it
+    data, addr = client_socket.recvfrom(1024)
+    print(data.decode())
+
+    start_new_thread(func1,(client_socket,))
+    start_new_thread(func2,(client_socket,))
+
+
+def func1(client_socket):
+   while True:
+        if msvcrt.kbhit():
+            line=msvcrt.getch()
+            client_socket.send(line)
+
+def func2(client_socket):
+    client_socket.settimeout(25)
+    try:
+        while True:
+            data, addr = client_socket.recvfrom(1024)
+    except:
+        print("wait to offer")
+        run_client()
+
+
+
+run_client()
+
